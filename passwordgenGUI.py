454f95fbe6ca49e36971c9c90ee8adcb5022c3f0
shanego416 <88070204+shanego416@users.noreply.github.com>
2023-02-05 17:06:21 -0500

    Add files via upload

---
+++ passwordgenGUI.py
@@ -0,0 +1,66 @@
+
+from tkinter import *
+from tkinter.ttk import Combobox
+import random
+
+screen = Tk()
+screen.title("Password Generator @tapgo416")
+screen.geometry('600x300')
+screen.configure(background ="black")
+
+
+def gen():
+    global sc1
+    sc1.set("")
+    passw=""
+    length=int(c1.get())
+    lowercase='abcdefghijklmnopqrstuvwxyz'
+    uppercase='ABCDEFGHIJKLMNOPQRSTUVWXYZ'+lowercase
+    mixs='0123456789'+lowercase+uppercase+'@#$%&*/?><.,'
+
+    if c2.get()=='Low Strength':
+        for i in range(0,length):
+            passw=passw+random.choice(lowercase)
+        sc1.set(passw)
+    elif c2.get()=='Medium Strength':
+        for i in range(0,length):
+            passw=passw+random.choice(uppercase)
+        sc1.set(passw)
+    elif c2.get()=='High Strength':
+        for i in range(0,length):
+            passw=passw+random.choice(mixs)
+        sc1.set(passw)
+
+sc1=StringVar('')
+
+t1=Label(screen,text='Automatic Password Generator',font=('Arial',25),fg='white',background ="black")
+t1.place(x=125,y=20)
+
+t2=Label(screen,text='Password:',font=('Arial',14),fg='white',background ="black")
+t2.place(x=145,y=90)
+
+il=Entry(screen,font=('Arial',14),textvariable=sc1)
+il.place(x=240,y=90)
+
+t3=Label(screen,text='Length: ',font=('Arial',14),fg='white',background ="black")
+t3.place(x=145,y=120)
+
+t4=Label(screen,text='Strength:',font=('Arial',14),fg='white',background ="black")
+t4.place(x=145,y=155)
+
+c1=Entry(screen,font=('Arial',14),width=10)
+c1.place(x=240,y=120)
+
+c2=Combobox(screen,font=('Arial',14),width=15)
+c2['values']=('Low Strength','Medium Strength','High Strength')
+c2.current(1)
+c2.place(x=240,y=155)
+
+b=Button(screen,text='Generate',font=('Arial',14),fg='red',background ="white",command=gen)
+b.place(x=240,y=195)
+
+
+screen.mainloop()
+
+
+
